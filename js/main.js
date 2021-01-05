7b565d5ae92f3a93b3a5a790b920fed6ae16506c
Riazzor <flo.sanika+@gmail.com>
2021-01-05 17:41:02 +0100

    tweet it button functionnal added spinning logo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const quoteB = document.getElementById('new-quote');
 const quoteP = document.getElementById('quote');
+
+const tweetB = document.getElementById('tweet');
+
 const adresse = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+const spin = document.querySelector('.spinner');
+
 
 quoteB.addEventListener('click', newQuote);
 
+
 async function newQuote() {
+      spin.classList.remove('hidden');
+      // let hiddenClass = Array.from(spin.classList).pop();
+      quoteB.disabled = true;
       
       try {
             const reponse = await fetch(adresse);
@@ -13,14 +22,27 @@ async function newQuote() {
             }
             const json = await reponse.json();
             console.log(json);
-
+            
             quoteP.innerHTML = '"<em>' + json.message + '</em>" <br> \
             <span style="display: inline-block;text-align: right; width: 100%;"><b>Donald TRUMP</b>' ;
       } catch (error) {
             console.log(error);
             alert("Check you internet connection");
+      } finally {
+            spin.classList.add('hidden');
+            quoteB.disabled = false;
       }
-
+      
+      tweetB.addEventListener('click', tweetIt);
+      
       // console.log(reponse);
+}
+
+function tweetIt(event) {
+      let quote = event.currentTarget.parentNode.querySelector('#quote').textContent.split('"')[1];
+      console.log(quote);
+      let url = `https://twitter.com/share?text=${quote} - Donald TRUMP`;
+      console.log(url);
+      window.open(url, '_blank', '');
 }
 
--- readme.txt
+++ readme.txt
@@ -3,3 +3,5 @@ will display a random quote at each click of the 'generate' button
 we'll start with Trump and see for more if it's fun
 
 TP of freshman tutorial
+https://freshman.tech/random-quote-machine/
+https://whatdoestrumpthink.com/api-docs/index.html
